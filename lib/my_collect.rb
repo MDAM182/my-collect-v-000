a6ede317e0550fe4296d26a670f57f3e11051782
Michael Morris <mdam2345@gmail.com>
2018-06-22 02:55:10 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,10 @@
 def my_collect(empty_array)
-  empty_array.each do |software|
-  software.upcase
+    i = 0
+    collection = []
+    while i < empty_array.length
+      collection << yield(array[i])
+      i += 1
+    end
+    collection
   end
-  my_collect(empty_array)
 end
